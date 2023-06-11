295127292748e40677054b62201cceb8d90b002a
Jahanzaib Tayyab <jtayyab204@gmail.com>
2023-06-11 20:38:48 +0500

    complete login and signUp page route

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -4,16 +4,18 @@ import { useFormik } from "formik";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { User } from "@/types/user";
+import { getError } from "@/utils/helper";
 import * as Yup from "yup";
 import en from "@/locales/en";
 import Input from "@/components/Input";
 
-const LoginSchema = Yup.object().shape({
+const SignUpSchema = Yup.object().shape({
+  name: Yup.string().required("Required"),
   email: Yup.string().email("Invalid email").required("Required"),
   password: Yup.string().min(6).required("Required"),
 });
 
-const SignUp = () => {
+const Login = () => {
   const router = useRouter();
 
   const userInfo = null;
@@ -25,17 +27,18 @@ const SignUp = () => {
 
   const formik = useFormik({
     initialValues: {
+      name: "",
       email: "",
       password: "",
     },
     enableReinitialize: true,
-    validationSchema: LoginSchema,
-    onSubmit: (values) => {
-      LoginHandler(values);
+    validationSchema: SignUpSchema,
+    onSubmit: (values, { resetForm }) => {
+      SignUpHandler(values);
     },
   });
 
-  async function LoginHandler(user: User) {
+  async function SignUpHandler(user: User) {
     const { email, password } = user;
     try {
       // const { data } = await axios.post("/api/users/login", {
@@ -45,22 +48,31 @@ const SignUp = () => {
       // dispatch(userInfoActions.userLogin(data));
       // jsCookie.set("userInfo", JSON.stringify(data));
       // router.push("/");
-    } catch (err: any) {}
+    } catch (err: any) {
+      console.log(getError(err));
+    }
   }
 
   return (
     <div className="flex flex-col items-center justify-center mt-8">
       <div className="w-full md:w-[50%] max-w-[500px] border-2 bg-palette-card shadow-lg py-4 px-8 rounded-lg">
-        <h2 className="text-lg md:text-2xl font-bold">Login</h2>
-        <p className="mt-4 mb-2">
-          {en.hi}
-          <br />
-          <span className="inline-block text-palette-mute dark:text-palette-base/80 text-[12px] mt-2 bg-palette-fill p-2">
-            {en.loginExplanation}
-          </span>
-        </p>
+        <h2 className="text-lg md:text-2xl font-bold">Sign Up</h2>
+        <p className="mt-4 mb-2">{en.hi}</p>
         <div className="py-5">
           <form onSubmit={formik.handleSubmit}>
+            <Input
+              id="name"
+              name="name"
+              type="text"
+              label={"User Name"}
+              placeholder="Please Enter Your Name"
+              required={true}
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.name && Boolean(formik.errors.name)}
+              helperText={formik.touched.name && formik.errors.name}
+            />
             <Input
               id="email"
               name="email"
@@ -91,19 +103,17 @@ const SignUp = () => {
               type="submit"
               className="bg-palette-primary w-full py-4 rounded-lg text-palette-side text-xl shadow-lg"
             >
-              Login
+              Sign Up
             </button>
           </form>
         </div>
       </div>
       <Link href={`/login`} className="block my-4">
-        <span className="text-sm text-palette-mute">
-          {en.alreadyHaveAnAccount}
-        </span>
+        <span className="text-sm text-palette-mute">{en.doHaveAnAccount}</span>
         <span className="text-cyan-500">Login</span>
       </Link>
     </div>
   );
 };
 
-export default SignUp;
+export default Login;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useImperativeHandle, useRef, useState } from "react";
 import en from "@/locales/en";
-
 interface Props {
   id: string;
   type: string;
@@ -15,31 +14,16 @@ interface Props {
   autocomplete?: string;
   title?: string;
   required?: boolean;
+  label?: string;
+  name?: string;
+  error?: boolean;
+  helperText?: string | boolean;
+  onChange?: (_event: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (_event: React.FocusEvent<HTMLInputElement, Element>) => void;
+  onFocus?: (_event: React.FocusEvent<HTMLInputElement, Element>) => void;
 }
 
-interface IImperativeHandler {
-  focus: () => void;
-  value?: string;
-}
-const Input = React.forwardRef<IImperativeHandler, Props>((props, ref) => {
-  const inputRef = useRef<HTMLInputElement | null>(null);
-  const [value, setValue] = useState(props.value || "");
-
-  function inputChangeHandler(e: React.FormEvent<HTMLInputElement>) {
-    setValue(e.currentTarget.value);
-  }
-
-  function inputFocused() {
-    inputRef.current?.focus();
-    inputRef.current?.setAttribute("style", "border:2px solid red");
-  }
-
-  useImperativeHandle(ref, () => {
-    return {
-      focus: inputFocused,
-      value: value,
-    };
-  });
+const Input: React.FC<Props> = (props) => {
   return (
     <div className="relative mb-8">
       <label
@@ -49,26 +33,59 @@ const Input = React.forwardRef<IImperativeHandler, Props>((props, ref) => {
         {props.required ? (
           <span className="text-rose-700 mx-1 mt-1">*</span>
         ) : null}
-        {en[`${props.id}`]}
+        {props.label}
       </label>
       <input
-        ref={inputRef}
         id={props.id}
         minLength={props.minLength}
         maxLength={props.maxLength}
         type={props.type}
-        placeholder={en[`${props.placeholder}`]}
-        value={value}
+        placeholder={props.placeholder ?? ""}
+        value={props.value}
         readOnly={props.readonly || false}
-        onChange={inputChangeHandler}
+        onChange={props.onChange}
+        onFocus={props.onFocus}
+        onBlur={props.onBlur}
         autoComplete={props.autocomplete || "off"}
         className="w-full py-4 px-6 border-[1px] border-gainsboro bg-palette-card outline-none rounded-lg shadow-md"
         title={props.title || ""}
         required={props.required || false}
       />
+      {props.error && (
+        <p className="text-sm text-red-700 capitalize">{props.helperText}</p>
+      )}
     </div>
   );
-});
+};
+// const Input = React.forwardRef<TextFieldPropsType>((props) => {
+//   return (
+//     <div className="relative mb-8">
+//       <label
+//         className="absolute -top-[30%] ltr:left-3 rtl:right-3 bg-palette-card p-[0.3rem] whitespace-nowrap"
+//         htmlFor={props.id}
+//       >
+//         {props.required ? (
+//           <span className="text-rose-700 mx-1 mt-1">*</span>
+//         ) : null}
+//         {label}
+//       </label>
+//       <input
+//         ref={inputRef}
+//         id={props.id}
+//         minLength={props.minLength}
+//         maxLength={props.maxLength}
+//         type={props.type}
+//         placeholder={en[`${props.placeholder}`]}
+//         value={value}
+//         readOnly={props.readonly || false}
+//         onChange={inputChangeHandler}
+//         autoComplete={props.autocomplete || "off"}
+//         className="w-full py-4 px-6 border-[1px] border-gainsboro bg-palette-card outline-none rounded-lg shadow-md"
+//         title={props.title || ""}
+//         required={props.required || false}
+//       />
+//     </div>
+//   );
+// });
 
-Input.displayName = "Input";
 export default Input;
