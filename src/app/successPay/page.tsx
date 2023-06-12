b7c8b605368515104184db2a37112673d2189ab5
Jahanzaib Tayyab <jtayyab204@gmail.com>
2023-06-12 18:08:48 +0500

    add custom success page

---
+++ src/app/successPay/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+import { runFireworks } from "@/utils/helper";
+import { Button } from "@/components/ui/button";
+
+const Success = () => {
+  useEffect(() => {
+    runFireworks();
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:order@example.com">
+            shopeonline@example.com
+          </a>
+        </p>
+        <Link href="/">
+          <Button>Continue Shopping</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
--- src/utils/helper/index.ts
+++ src/utils/helper/index.ts
@@ -1,4 +1,5 @@
 import { Product } from "@/types/products";
+import confetti from "canvas-confetti";
 
 export const calculateDiscountPercentage = (
   price: number,
@@ -67,4 +68,37 @@ export const newestProductsFn = (products: Product[]) => {
     };
   });
   return productsWithTimeStamp.sort(sortByTimeStamp);
+};
+
+export const runFireworks = () => {
+  var duration = 5 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  function randomInRange(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const interval: any = setInterval(function () {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    var particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      })
+    );
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      })
+    );
+  }, 250);
 };
